11dde0d00d7ba4f198394eaaa9c1107dfacf4ab3
max14632 <maxsilkin@gmail.com>
2020-06-04 05:20:57 +0000

    front page done

--- base/base/urls.py
+++ base/base/urls.py
@@ -19,10 +19,10 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('business/admin/', admin.site.urls),
-    path('business/register/', user_views.register, name='register'),
-    path('business/profile/', user_views.profile, name='profile'),
-    path('business/login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('business/logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
